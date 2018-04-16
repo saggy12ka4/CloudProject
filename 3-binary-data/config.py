1432b8eaae167e8638da2a48e39639ba69f80ab5
saggy12ka4 <rathodsagar12ka4@gmail.com>
2018-04-17 01:11:21 +0530

    Update config.py

--- 3-binary-data/config.py
+++ 3-binary-data/config.py
@@ -13,7 +13,7 @@ DATA_BACKEND = 'datastore'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'navin-201209'
+PROJECT_ID = 'valid-progress-201319'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
@@ -24,7 +24,7 @@ CLOUDSQL_DATABASE = 'bookshelf'
 # Set this value to the Cloud SQL connection name, e.g.
 #   "project:region:cloudsql-instance".
 # You must also update the value in app.yaml.
-CLOUDSQL_CONNECTION_NAME = 'navin-201209:us-central1:saggy'
+CLOUDSQL_CONNECTION_NAME = 'valid-progress-201319:us-central1:me'
 
 # The CloudSQL proxy is used locally to connect to the cloudsql instance.
 # To start the proxy, use:
@@ -71,6 +71,6 @@ MONGO_URI = 'mongodb://user:password@host:27017/database'
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = 'staging.navin-201209.appspot.com'
+CLOUD_STORAGE_BUCKET = 'staging.valid-progress-201319.appspot.com'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
